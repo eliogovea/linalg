323a14369fd08968f66dfd133cfb675e3790f5e9
Elio Govea <eliogovea1993@gmail.com>
2020-03-06 18:00:49 -0500

    add + and - operators

--- matrix.hpp
+++ matrix.hpp
@@ -31,6 +31,30 @@ public:
     return values_[r * N + c];
   }
 
+  template <typename To, size_t No, size_t Mo>
+  auto operator + (const matrix<To, No, Mo>& rhs) {
+    static_assert(N == No && M == Mo, "dimensions must match to perform addition");
+    auto result = matrix_factory<decltype(std::declval<T>() + std::declval<To>()), N, M>::zero();
+    for (size_t r = 0; r < N; r++) {
+      for (size_t c = 0; c < M; c++) {
+        result.at(r, c) = at(r, c) + rhs.at(r, c);
+      }
+    }
+    return result;
+  }
+
+  template <typename To, size_t No, size_t Mo>
+  auto operator - (const matrix<To, No, Mo>& rhs) {
+    static_assert(N == No && M == Mo, "dimensions must match to perform addition");
+    auto result = matrix_factory<decltype(std::declval<T>() + std::declval<To>()), N, M>::zero();
+    for (size_t r = 0; r < N; r++) {
+      for (size_t c = 0; c < M; c++) {
+        result.at(r, c) = at(r, c) - rhs.at(r, c);
+      }
+    }
+    return result;
+  }
+
 private:
 
   std::array<T, N * M> values_;
